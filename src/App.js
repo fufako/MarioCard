2c3f138d8fcb99e5a471598a5b51be99daf649b4
fufako <patryko.mikiciuk@gmail.com>
2022-09-13 15:20:42 +0200

    Add GameBoard componenet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Scoreboard from "./components/Scoreboard"
+import GameBoard from "./components/GameBoard"
 
 function App() {
   const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -7,6 +8,7 @@ function App() {
     <div className="main">
       <Header />
       <Scoreboard />
+      <GameBoard />
     </div>
   )
 }
---
+++ src/components/GameBoard.js
@@ -0,0 +1,70 @@
+const GameBoard = () => {
+  return (
+    <div className="game-board">
+      <div className="container">
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title">.</p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+        <div className="card">
+          <img src="" alt="" />
+          <p className="title"></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GameBoard
